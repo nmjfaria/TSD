981dbc9af8e376a07ff3c9fb4c91f1dd5dbd9448
Nelson Faria <nmjfaria@gmail.com>
2022-06-19 21:51:20 +0200

    01 added

---
+++ 01-Introduction/final/alarm.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+public class RobotOnMoon
+{
+    public string isSafeCommand(string[] board, string S)
+    {
+		int[] currentPos = new int[0];
+		for (int i = 0; i < board.Length; i++){
+			for(int j = 0; j < board[i].Length;j++)
+			{
+				if (board[i][j] == 'S'){
+					currentPos = new int[] { i, j };
+					break;
+				}
+			}
+			if(currentPos.Length != 0)
+					break;
+		}
+		
+		for(int i = 0; i < S.Length; i++){
+			int[] nextPos = new int[2];
+			currentPos.CopyTo(nextPos, 0);
+			switch(S[i]){
+				case 'U':
+					nextPos[0] -= 1;
+					break;
+				case 'D':
+					nextPos[0] += 1;
+					break;
+				case 'L':
+					nextPos[1] -= 1;
+					break;
+				case 'R':
+					nextPos[1] += 1;
+					break;
+			}
+			
+			if(nextPos[0] < 0 || nextPos[1] < 0 || nextPos[0] >= board[0].Length || nextPos[1] >= board[0].Length)
+				return "Dead";
+			
+			if(board[nextPos[0]][nextPos[1]] != '#')
+			{
+				nextPos.CopyTo(currentPos, 0);
+				currentPos = nextPos;
+			}
+				
+		}
+        return "Alive";
+    }
+
+    #region Testing code
+
+    [STAThread]
+    private static Boolean KawigiEdit_RunTest(int testNum, string[] p0, string p1, Boolean hasAnswer, string p2)
+    {
+        Console.Write("Test " + testNum + ": [" + "{");
+        for (int i = 0; p0.Length > i; ++i)
+        {
+            if (i > 0)
+            {
+                Console.Write(",");
+            }
+            Console.Write("\"" + p0[i] + "\"");
+        }
+        Console.Write("}" + "," + "\"" + p1 + "\"");
+        Console.WriteLine("]");
+        RobotOnMoon obj;
+        string answer;
+        obj = new RobotOnMoon();
+        DateTime startTime = DateTime.Now;
+        answer = obj.isSafeCommand(p0, p1);
+        DateTime endTime = DateTime.Now;
+        Boolean res;
+        res = true;
+        Console.WriteLine("Time: " + (endTime - startTime).TotalSeconds + " seconds");
+        if (hasAnswer)
+        {
+            Console.WriteLine("Desired answer:");
+            Console.WriteLine("\t" + "\"" + p2 + "\"");
+        }
+        Console.WriteLine("Your answer:");
+        Console.WriteLine("\t" + "\"" + answer + "\"");
+        if (hasAnswer)
+        {
+            res = answer == p2;
+        }
+        if (!res)
+        {
+            Console.WriteLine("DOESN'T MATCH!!!!");
+        }
+        else if ((endTime - startTime).TotalSeconds >= 2)
+        {
+            Console.WriteLine("FAIL the timeout");
+            res = false;
+        }
+        else if (hasAnswer)
+        {
+            Console.WriteLine("Match :-)");
+        }
+        else
+        {
+            Console.WriteLine("OK, but is it right?");
+        }
+        Console.WriteLine("");
+        return res;
+    }
+
+    public static void Main(string[] args)
+    {
+        Boolean all_right;
+        all_right = true;
+
+        string[] p0;
+        string p1;
+        string p2;
+
+        // ----- test 0 -----
+        p0 = new string[] {".....", ".###.", "..S#.", "...#."};
+        p1 = "URURURURUR";
+        p2 = "Alive";
+        all_right = KawigiEdit_RunTest(0, p0, p1, true, p2) && all_right;
+        // ------------------
+
+        // ----- test 1 -----
+        p0 = new string[] {".....", ".###.", "..S..", "...#."};
+        p1 = "URURURURUR";
+        p2 = "Dead";
+        all_right = KawigiEdit_RunTest(1, p0, p1, true, p2) && all_right;
+        // ------------------
+
+        // ----- test 2 -----
+        p0 = new string[] {".....", ".###.", "..S..", "...#."};
+        p1 = "URURU";
+        p2 = "Alive";
+        all_right = KawigiEdit_RunTest(2, p0, p1, true, p2) && all_right;
+        // ------------------
+
+        // ----- test 3 -----
+        p0 = new string[] {"#####", "#...#", "#.S.#", "#...#", "#####"};
+        p1 = "DRULURLDRULRUDLRULDLRULDRLURLUUUURRRRDDLLDD";
+        p2 = "Alive";
+        all_right = KawigiEdit_RunTest(3, p0, p1, true, p2) && all_right;
+        // ------------------
+
+        // ----- test 4 -----
+        p0 = new string[] {"#####", "#...#", "#.S.#", "#...#", "#.###"};
+        p1 = "DRULURLDRULRUDLRULDLRULDRLURLUUUURRRRDDLLDD";
+        p2 = "Dead";
+        all_right = KawigiEdit_RunTest(4, p0, p1, true, p2) && all_right;
+        // ------------------
+
+        // ----- test 5 -----
+        p0 = new string[] {"S"};
+        p1 = "R";
+        p2 = "Dead";
+        all_right = KawigiEdit_RunTest(5, p0, p1, true, p2) && all_right;
+        // ------------------
+
+        if (all_right)
+        {
+            Console.WriteLine("You're a stud (at least on the example cases)!");
+        }
+        else
+        {
+            Console.WriteLine("Some of the test cases had errors.");
+        }
+    }
+
+    #endregion
+}
